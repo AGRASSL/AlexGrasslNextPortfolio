d010d8ab67c081dac2a89bcc2fe504ca9a8ee257
Alexandra Grassl <eai.alexandra@gmail.com>
2021-08-13 10:28:37 -0500

    project work added w/o pics

--- Constants/projects.js
+++ Constants/projects.js
@@ -12,29 +12,27 @@ import PROJECT3_3 from '../styles/projects/Project3_3.jpg';
 // first 3 projects are visible on the homepage while all are visible on the work page.
 
 export const projects = [
-    //leave the githublink field blank if it is a private repo or doent exist, alerts will take care of it
-    //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
     {
-        name: 'Dunder Mifflin', //project name
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum', //project description
-        githubLink: '', //github repo link
-        projectLink: '', //deployed project link
-        tech: ['Tech1', 'Tech2', 'Tech3'], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
+        name: 'Fridge Foodie', 
+        description: 'Front-end application to minimize food waste while discovering new recipes-- and with a wine pairing!', 
+        githubLink: 'https://github.com/AGRASSL/FridgeFoodieRecipes',
+        projectLink: 'https://agrassl.github.io/FridgeFoodieRecipes/index.html',
+        tech: ['Javascript', 'HTML', 'CSS'], 
         photo: [PROJECT1_1, PROJECT1_2, PROJECT1_3], //list of names of images from above imports.
     },
     {
-        name: 'Project 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
-        tech: ['Tech1', 'Tech2', 'Tech3'],
+        name: 'Zanes Magical Gathering',
+        description: 'A game aid for the one and only, Magic: The Gathering. Being able to search and save legal cards, keep track of lives, 1+1s, tokens, and record you game statistics to your personal profile.',
+        githubLink: 'https://github.com/AGRASSL/zanes-magical-gathering',
+        projectLink: 'http://zanelandia.herokuapp.com/',
+        tech: ['Next,js', 'SASS', 'MongoDB'],
         photo: [PROJECT2_1, PROJECT2_2, PROJECT2_3],
     },
     {
-        name: 'Project 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
+        name: 'Note Taker',
+        description: 'Online application to have the ability to take notes, save notes, and remove them once completed or when desired.',
+        githubLink: 'https://github.com/AGRASSL/NoteTaker',
+        projectLink: 'https://notetakeralex.herokuapp.com/',
         tech: ['Tech1', 'Tech2', 'Tech3'],
         photo: [PROJECT3_1, PROJECT3_2, PROJECT3_3],
     },
